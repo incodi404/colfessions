95a55ed5d8671557c1a2ba342ce6ddc4efcb91b1
incodi404 <dipankarnn2123@gmail.com>
2024-07-28 21:09:55 +0530

    op

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,2 @@
-export const SERVER_LINK = "https://colfessions-backend.onrender.com/api/v1"
-export const AUTH_LINK = "https://colfessions-backend.onrender.com"
-// export const SERVER_LINK = "https://colfessions-backend.vercel.app/api/v1"
-// export const AUTH_LINK = "https://colfessions-backend.vercel.app"
+export const SERVER_LINK = "https://colfessions-backend.vercel.app/api/v1"
+export const AUTH_LINK = "https://colfessions-backend.vercel.app"
